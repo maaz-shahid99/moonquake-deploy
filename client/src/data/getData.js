f6ff00d0a3f8985787c926a4fe27b75e100d694a
vishalkrsharma <vishalkrsharma1234@gmail.com>
2022-10-02 13:50:03 +0530

    getData changes

--- client/src/data/getData.js
+++ client/src/data/getData.js
@@ -4,7 +4,7 @@ export const getData = async () => {
   try {
     const res = await axios({
       method: 'GET',
-      url: '/get',
+      url: '/api/moonQuake/get',
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
